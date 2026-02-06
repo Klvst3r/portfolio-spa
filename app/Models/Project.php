0ef6b35040747525c83c7a5a80d10c5359cf1843
Klvst3r <klvst3r@gmail.com>
2026-02-06 10:43:37 -0600

    Se corrije el Modelo de Project para coindicir con los campos definidos en la migracion

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -1,13 +1,9 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\File;
-
-
-
 
 class Project extends Model
 {
